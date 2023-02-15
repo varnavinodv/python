83200c822b787435e9e75ad8b6bf26fcad1f55bf
varnavinodv <116860438+varnavinodv@users.noreply.github.com>
2023-02-15 14:05:37 +0530

    Add files via upload

---
+++ CO4/graphics/circle.py
@@ -0,0 +1,5 @@
+from math import pi
+def area_circle(radius):
+     return pi*radius*radius
+def perimeter_circle(radius):
+     return 2*pi*radius
---
+++ CO4/graphics/tdgraphics/sphere.py
@@ -0,0 +1,5 @@
+from math import pi
+def area_sphere(radius):
+    return 4*(pi*radius*radius)
+def perimeter_sphere(radius):
+    return 2*pi*radius
